019a0822452c2ce98d01edd9b7bd50c6cca0774b
Victor Lopes <viclopes2621@gmail.com>
2020-04-24 02:53:50 -0300

    Changes to Parallax.js

--- resources/parallax.js
+++ resources/parallax.js
@@ -1,8 +1,4 @@
 function castParallax() {
-
-	var opThresh = 350;
-	var opFactor = 750;
-
 	window.addEventListener("scroll", function(event){
 
 		var top = this.pageYOffset;
@@ -20,40 +16,8 @@ function castParallax() {
 
 }
 
-function dispelParallax() {
-	jQuery("#nonparallax").css('display','block');
-	jQuery("#parallax").css('display','none');
-}
-
-function castSmoothScroll() {
-	jQuery.srSmoothscroll({
-		step: 80,
-		speed: 300,
-		ease: 'linear'
-	});
-}
-
 jQuery(document).ready(function($) {
 
-	// Set menu
-	$('.menu-toggle').on('click', function(e){
-		e.preventDefault();
-		$('#nav .menu').slideToggle();
-		$(this).toggleClass('open');
-	});
-
-	var platform = navigator.platform.toLowerCase();
-	var userAgent = navigator.userAgent.toLowerCase();
-
-	if ( platform.indexOf('ipad') != -1  ||  platform.indexOf('iphone') != -1 ) {
-		dispelParallax();
-	}	else if (platform.indexOf('win32') != -1 || platform.indexOf('linux') != -1) {
-		castParallax();
-		if ($.browser.webkit)	{
-			castSmoothScroll();
-		}
-	}	else {
-		castParallax();
-	}
+	castParallax();
 
 });
